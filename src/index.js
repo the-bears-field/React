9995ba519a54ef0a9fbe6f057e5fd40981617b34
the-bears-field <thebearsfield+github@gmail.com>
2022-09-07 06:14:43 +0900

    TODOアプリの追加機能を実装
    htmlにJavaScript実行に必要な要素に対しIDを付与
    JavaScriptに追加ボタンをクリックした時のイベント処理と、実行する関数を追加
    
    参考URL
    https://www.udemy.com/course/modern_javascipt_react_beginner/learn/lecture/21899442
    https://jsprimer.net/use-case/ajaxapp/display/#html-to-dom
    https://jsprimer.net/use-case/todoapp/form-event/#input-to-todolist
    https://jsprimer.net/use-case/todoapp/event-model/#event-emitter
    https://jsprimer.net/use-case/todoapp/final/#TodoItemView

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import "./index.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得。
+  const inputedText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = '';
+
+  // liタグ作成
+  const liTag = document.createElement('li');
+  liTag.className = 'list__row';
+
+  // pタグ作成
+  const pTag = document.createElement('p');
+  pTag.className = 'list__title';
+  pTag.innerText = inputedText;
+
+  // 完了ボタン作成
+  const completeButton = document.createElement('button');
+  completeButton.className = 'button';
+  completeButton.innerText = '完了';
+
+  // 完了ボタンを押したときの処理
+  completeButton.addEventListener('click', () => {
+    alert('完了');
+  });
+
+  // 削除ボタン作成
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'button';
+  deleteButton.innerText = '削除';
+
+  // 削除ボタンを押したときの処理
+  deleteButton.addEventListener('click', () => {
+    alert('削除');
+  });
+
+  // liタグの子要素にpタグを追加
+  liTag.appendChild(pTag);
+  liTag.appendChild(completeButton);
+  liTag.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById('imcomplete-list').appendChild(liTag);
+};
+
+document.getElementById('add-button').addEventListener('click', () => onClickAdd());
