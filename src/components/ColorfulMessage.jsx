916b27028eb7f46b79d36863e4421b5938366b27
the-bears-field <thebearsfield+github@gmail.com>
2022-09-13 13:50:11 +0900

    コンポーネントの出力方法を変更
    ColorfulMessageコンポーネントのpropsの要素を分割代入を用いて変数に代入
    メッセージの出力方法をprops.childrenを使った出力方法に変更
    
    参考URL
    https://www.udemy.com/course/modern_javascipt_react_beginner/learn/lecture/21899530#learning-tools
    https://ja.reactjs.org/docs/components-and-props.html#gatsby-focus-wrapper
    https://ja.reactjs.org/docs/glossary.html#propschildren

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color: color,
     fontSize: '18px'
   };
 
   return (
-    <p style={ contentStyle }>{ props.message }</p>
+    <p style={ contentStyle }>{ children }</p>
   );
 };
 
