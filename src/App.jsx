3c51e89e9559e7c07803a63e7e5e43cb757a7464
the-bears-field <thebearsfield+github@gmail.com>
2022-09-20 14:18:09 +0900

    Reactを使ったTODOアプリ作成に向け、CSSを定義
    内容は前回の使い回し
    
    参考URL:
    https://www.udemy.com/course/modern_javascipt_react_beginner/learn/lecture/22607906

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from "react";
+import './index.css';
 
 export const App = () => {
   return (
   <>
-    <div>
-      <input placeholder="TODOを入力" />
-      <button>追加</button>
+    <div className="input-area">
+      <input className="input-area__input" placeholder="TODOを入力" />
+      <button className="button">追加</button>
     </div>
-    <div>
-      <p>未完了のTODO</p>
-      <ul>
-        <li>
-            <p>あああ</p>
-            <button>完了</button>
-            <button>削除</button>
+    <div className="imcomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul className="list">
+        <li className="list__row">
+            <p className="list__title">あああ</p>
+            <button className="button">完了</button>
+            <button className="button">削除</button>
         </li>
-        <li>
-            <p>いいい</p>
-            <button>完了</button>
-            <button>削除</button>
+        <li className="list__row">
+            <p className="list__title">いいい</p>
+            <button className="button">完了</button>
+            <button className="button">削除</button>
         </li>
       </ul>
     </div>
-    <div>
-      <p>完了のTODO</p>
-      <ul>
-        <li>
-            <p>あああ</p>
-            <button>戻す</button>
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul className="list">
+        <li className="list__row">
+            <p className="list__title">あああ</p>
+            <button className="button">戻す</button>
         </li>
-        <li>
-            <p>いいい</p>
-            <button>戻す</button>
+        <li className="list__row">
+            <p className="list__title">いいい</p>
+            <button className="button">戻す</button>
         </li>
       </ul>
     </div>
