af079cadcb229eda599ddd4e67d361d01ce165bd
the-bears-field <thebearsfield+github@gmail.com>
2022-09-20 14:07:38 +0900

    Reactを使ったTODOアプリ作成に向け、構造を作成
    参考URL:
    https://www.udemy.com/course/modern_javascipt_react_beginner/learn/lecture/22607900

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 import React, { useEffect, useState } from "react";
 
 export const App = () => {
-  return <div></div>;
+  return (
+  <>
+    <div>
+      <input placeholder="TODOを入力" />
+      <button>追加</button>
+    </div>
+    <div>
+      <p>未完了のTODO</p>
+      <ul>
+        <li>
+            <p>あああ</p>
+            <button>完了</button>
+            <button>削除</button>
+        </li>
+        <li>
+            <p>いいい</p>
+            <button>完了</button>
+            <button>削除</button>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>完了のTODO</p>
+      <ul>
+        <li>
+            <p>あああ</p>
+            <button>戻す</button>
+        </li>
+        <li>
+            <p>いいい</p>
+            <button>戻す</button>
+        </li>
+      </ul>
+    </div>
+  </>
+  );
 };
