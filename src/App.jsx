3a5e33e55537d8c2018c7b18bc08eb8b14c166e8
the-bears-field <thebearsfield+github@gmail.com>
2022-09-28 14:12:37 +0900

    TODOの上限件数を設定
    Appコンポーネントのjsxにpタグおよびメッセージを追加
    incompleteTodosの要素数を判定し表示する処理を追加
    InputTodoコンポーネントにdisabled属性を追加、
    要素の内容はincompleteTodosの要素数の判定結果を設定
    InputTodo関数コンポーネント中の分割代入に変数(disabled)を追加
    InputTodo関数コンポーネントのjsxのinputタグとbuttonタグにdisabled属性を追加
    
    参考URL
    https://www.udemy.com/course/modern_javascipt_react_beginner/learn/lecture/22644585

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,14 @@ export const App = () => {
       todoText={todoText}
       onChange={onChangeTodoText}
       onClick={onClickAdd}
+      disabled={incompleteTodos.length >= 5}
     />
+    {
+      incompleteTodos.length >= 5 &&
+      <p style={{ color: 'red' }}>
+        登録できるタスクは5個までです。消化してください。
+      </p>
+    }
     <IncompleteTodos
       todos={incompleteTodos}
       onClickComplete={onClickComplete}
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
 
   return (
     <div className="input-area">
-      <input className="input-area__input" id="add-text" onChange={onChange} placeholder="TODOを入力" value={todoText} />
-      <button className="button" id="add-button" onClick={onClick}>追加</button>
+      <input className="input-area__input" disabled={disabled} id="add-text" onChange={onChange} placeholder="TODOを入力" value={todoText} />
+      <button className="button" disabled={disabled} id="add-button" onClick={onClick}>追加</button>
     </div>
   );
 };
