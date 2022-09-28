262808ad7bcc890f4c35a3631de5f684997882ae
the-bears-field <thebearsfield+github@gmail.com>
2022-09-28 14:43:26 +0900

    次の学習に向けて内容をリセット

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { CompleteTodos } from "./components/CompleteTodos";
-import { IncompleteTodos } from "./components/IncompleteTodos";
-import { InputTodo } from './components/InputTodo';
 import './index.css';
 
 export const App = () => {
-  const [ todoText, setTodoText ] = useState('');
-
-  const [ incompleteTodos, setIncompleteTodos ] = useState([]);
-
-  const [ completeTodos, setCompleteTodos ] = useState([]);
-
-  const onChangeTodoText = (event) => setTodoText(event.target.value);
-
-  const onClickAdd = () => {
-    if (!todoText) return;
-    addIncompeteTodosElement(todoText);
-    setTodoText('');
-  };
-
-  const onClickDelete = (index) => {
-    deleteIncompeteTodosElement(index);
-  };
-
-  const onClickComplete = (index) => {
-    deleteIncompeteTodosElement(index);
-
-    const newCompeleteTodos = [...completeTodos, incompleteTodos[index]];
-    setCompleteTodos(newCompeleteTodos);
-  };
-
-  const onClickBack = (index) => {
-    const newCompeleteTodos = [...completeTodos];
-    newCompeleteTodos.splice(index, 1);
-    setCompleteTodos(newCompeleteTodos);
-    addIncompeteTodosElement(completeTodos[index]);
-  };
-
-  const deleteIncompeteTodosElement = (index) => {
-    const newIncompeteTodos = [...incompleteTodos];
-    newIncompeteTodos.splice(index, 1);
-    setIncompleteTodos(newIncompeteTodos);
-  };
-
-  const addIncompeteTodosElement = (todoText) => {
-    const newIncompeteTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newIncompeteTodos);
-  };
-
   return (
-  <>
-    <InputTodo
-      todoText={todoText}
-      onChange={onChangeTodoText}
-      onClick={onClickAdd}
-      disabled={incompleteTodos.length >= 5}
-    />
-    {
-      incompleteTodos.length >= 5 &&
-      <p style={{ color: 'red' }}>
-        登録できるタスクは5個までです。消化してください。
-      </p>
-    }
-    <IncompleteTodos
-      todos={incompleteTodos}
-      onClickComplete={onClickComplete}
-      onClickDelete={onClickDelete}
-    />
-    <CompleteTodos
-      todos={completeTodos}
-      onClickBack={onClickBack}
-    />
-  </>
+    <>
+    </>
   );
-};
+}
