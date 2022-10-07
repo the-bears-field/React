e4ae52d1e2db0bca7c5db9ccfdf3ddf104b0e815
the-bears-field <thebearsfield+github@gmail.com>
2022-10-07 14:55:23 +0900

    CSS反映方法の学習のため初期化
    https://www.udemy.com/course/react_stepup/learn/lecture/24823340

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-import React, { useState, useCallback } from 'react';
-import { ChildArea } from './ChildArea';
+import React from 'react';
 import './index.css';
 
 export const App = () => {
-  const [ text, setText ] = useState('');
-  const [ isOpen, setIsOpen ] = useState(false);
-
-  const style = {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '2vw auto',
-    width: '20vw'
-  };
-
-  const onChangeText = (event) => setText(event.target.value);
-
-  const onClickOpen = () => setIsOpen(!isOpen);
-  const onClickClose = useCallback(() => setIsOpen(false), [setIsOpen]);
-
-  return (
-    <div style={style}>
-      <input type="text" value={text} onChange={onChangeText} />
-      <button style={{ marginTop: '2vw' }} onClick={onClickOpen}>表示</button>
-      <ChildArea isOpen={isOpen} onClickClose={onClickClose} />
-    </div>
-  );
 }
