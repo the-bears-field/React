5aaca834f381a1fd83263b15048a84bd2276b3b1
the-bears-field <thebearsfield+github@gmail.com>
2022-10-17 15:03:29 +0900

    Inline Styleでのスタイル付けを実装。
    InlineStyle.jsxを新規作成し、InlineStyleコンポーネント生成処理を追加
    App.jsxにInlineStyleコンポーネントのインポート処理を追加
    Appコンポーネントの返り値にInlineStyleを挿入
    
    https://www.udemy.com/course/react_stepup/learn/lecture/24823340

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './index.css';
+import { InlineStyle } from './components/InlineStyle';
 
 export const App = () => {
+  return (
+    <div>
+      <InlineStyle />
+    </div>
+  );
 }
