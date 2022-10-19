6c27cddd02682e64e223ab55315ebb18086455b9
the-bears-field <thebearsfield+github@gmail.com>
2022-10-19 14:27:31 +0900

    CSS Modulesに対応したコンポーネントを追加
    scssファイルを追加し、CSSを記述
    CssModules.jsxを新規作成
    scssファイルのインポート処理とCssModulesコンポーネント生成処理を追加
    App.jsxにCssModulesコンポーネントのインポート処理を追加
    AppコンポーネントにCssModulesコンポーネント出力処理を追加
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823344

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/CssModules';
 
 export const App = () => {
   return (
     <div>
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
