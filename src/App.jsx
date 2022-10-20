41e071c116bc14963f8f9c637ccd8f1df30fe540
the-bears-field <thebearsfield+github@gmail.com>
2022-10-20 21:29:47 +0900

    Styled JSXに対応したコンポーネントを追加
    StyledJsx.jsxを新規作成
    StyledJsxコンポーネント生成し、返り値の一部にstyleタグを追加
    styleタグにjsx属性を付与
    Javascriptのテンプレートリテラル内にCSSを記述
    App.jsxにStyledJsx.jsxのインポート処理を追加
    Appコンポーネントの返り値にStyledJsxコンポーネントを追加
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823348
    https://github.com/vercel/styled-jsx
    https://inside.dmm.com/entry/2018/05/14/react-styled-jsx
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Template_literals

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
 
 export const App = () => {
   return (
     <div>
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
