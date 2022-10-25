a610118db7afd6d6d8c89dd83f26854bb3a83ed1
the-bears-field <thebearsfield+github@gmail.com>
2022-10-25 14:35:53 +0900

    基本的なページ遷移を実装
    react-router-domモジュールから必要な名前つきエクスポートを選択してインポート
    前回作成したコンポーネントをインポート
    styled-componentsをインポート
    
    StyledAppコンポーネントを作成して、テンプレートリテラル内でCSSを記述
    
    Appコンポーネントの返り値を変更
    最上層はBrowserRouterコンポーネントを使用
    2層目はStyledAppコンポーネントを使用
    3層目はLinkコンポーネントをSwitchコンポーネントを並べて使用
    Linkコンポーネントにto属性を付与し、パスを記述
    Switchコンポーネント内にそれぞれRouteコンポーネントを記述
    Routeコンポーネント内に前回作成したコンポーネントを記述し、path属性を付与
    ルートパスにあたるRouteコンポーネントにexact属性を付与
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823412
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/import
    https://jsprimer.net/basic/module/#es-module-syntax
    https://qiita.com/ShinKano/items/541050c36e08e78a5176
    https://v5.reactrouter.com/web/api/BrowserRouter
    https://v5.reactrouter.com/web/api/Link
    https://v5.reactrouter.com/web/api/Switch
    https://v5.reactrouter.com/web/api/Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
+import styled from 'styled-components';
+
 
 export const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <StyledApp>
+        <Link to="/">Home</Link>
+        <Link to="/page1">Page1</Link>
+        <Link to="/page2">Page2</Link>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/page1">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+        </Switch>
+      </StyledApp>
+    </BrowserRouter>
   );
 }
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
