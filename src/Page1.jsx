9faf4dbf73fff9ea3150df5406d1a08592fae79d
the-bears-field <thebearsfield+github@gmail.com>
2022-10-26 14:26:21 +0900

    ネストされたページ遷移を実装
    Page1.jsxでstyled-componentsモジュールと
    react-router-domモジュールからLinkをインポート
    StyledPage1コンポーネントを作成し、テンプレートリテラル内でCSSを記述
    Page1コンポーネントの返り値を変更
    最上層をStyledPage1コンポーネントを使用
    第2下層のキャプションの下部にLinkコンポーネントを配置
    to属性を付与し、それぞれリンクを指定
    
    App.jsxで前回作成したコンポーネントをインポート
    Appコンポーネントの返り値の一部を変更
    page1へのアクセスを担うRouteコンポーネントの中身と終了タグを削除し、セルフクロージング形式に変更
    render属性を追加し、値としてアロー関数を定義
    引数としてpropsのmatchキーのurlの値を分割代入で抽出して使用
    その返り値として、最上層にSwitchコンポーネント、第2層にRouteコンポーネントを複数使用したJSXを指定
    Routeコンポーネントにpath属性を付与し、それぞれpage1、前回作成したコンポーネントへのアクセス情報を記述
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823414
    https://v5.reactrouter.com/web/api/Route/route-render-methods

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 export const Page1 = () => {
   return (
-    <div>
+    <StyledPage1>
       <p>Page1ページです</p>
-    </div>
+      <Link to="/page1/detailA">DetailA</Link>
+      <Link to="/page1/detailB">DetailB</Link>
+    </StyledPage1>
   );
 }
+
+const StyledPage1 = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
