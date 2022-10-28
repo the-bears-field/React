9b2c07ca6ee0415947631d8d88c9a99b50b1d514
the-bears-field <thebearsfield+github@gmail.com>
2022-10-28 13:32:44 +0900

    ルート定義を分割
    routerディレクトリを新規作成
    その配下にRouter.jsxを新規作成し、Routerコンポーネントを作成
    App.jsxからSwitchコンポーネントで囲われた箇所をRouterコンポーネントの返り値に移動
    移動に際し、必要となる名前つきエクスポートをインポート
    
    App.jsxで不要となったコンポーネント、名前つきエクスポートのインポート処理を削除
    Routerコンポーネントをインポート
    Switchコンポーネントがあった箇所をRouterコンポーネントに置換
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823418

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
-import { Page2 } from './Page2';
 import styled from 'styled-components';
+import { Router } from './router/Router';
 
 
 export const App = () => {
@@ -16,29 +12,7 @@ export const App = () => {
         <Link to="/">Home</Link>
         <Link to="/page1">Page1</Link>
         <Link to="/page2">Page2</Link>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route
-            path="/page1"
-            render={({ match: { url } }) => (
-              <Switch>
-                <Route exact path={url}>
-                  <Page1/>
-                </Route>
-                <Route path={`${url}/detailA`}>
-                  <Page1DetailA />
-                </Route>
-                <Route path={`${url}/detailB`}>
-                  <Page1DetailB />
-                </Route>
-              </Switch>
-          )} />
-          <Route path="/page2">
-            <Page2 />
-          </Route>
-        </Switch>
+        <Router />
       </StyledApp>
     </BrowserRouter>
   );
