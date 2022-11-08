7cd30a29d25ee1888aa11279c4f67277f72edccb
the-bears-field <thebearsfield+github@gmail.com>
2022-11-08 14:53:05 +0900

    情報を保持した状態でのページ遷移を学習
    Page1コンポーネント中で配列を定義
    /page1/detailAへの遷移を担うLinkコンポーネントのto属性の値をオブジェクトに変更
    pathnameキーの値に遷移先のパスを文字列で定義
    stateキーの値に受け渡したい情報を定義
    
    Page1DetailA.jsxにreact-router-domからuseLocationフックをインポート
    Page1DetailAコンポーネント内でuseLocationフックを使いstateの値を抽出し、変数(state)に格納
    console.logで値が継承されているか確認した
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823424
    https://v5.reactrouter.com/web/api/Hooks/uselocation
    https://v5.reactrouter.com/web/api/location

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
   return (
     <StyledPage1>
-      <p>Page1ページです</p>
-      <Link to="/page1/detailA">DetailA</Link>
+      <h1>Page1ページです</h1>
+      <Link to={{
+        pathname: '/page1/detailA',
+        state: arr
+      }}>
+        DetailA
+      </Link>
       <Link to="/page1/detailB">DetailB</Link>
     </StyledPage1>
   );
