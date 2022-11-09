8e9da13a50712c7a89daffac5887617db341347a
the-bears-field <thebearsfield+github@gmail.com>
2022-11-09 14:10:41 +0900

    Linkコンポーネント使用しないページ遷移を学習
    Page1.jsxを編集
    react-router-domからuseHistoryフックをインポート
    Page1コンポーネント内でuseHistoryフックを使用し、historyインスタンスを生成
    その後、関数(onClickDetailA)を定義し、historyインスタンスのpushメソッドを実行するよう記述
    返り値のjsxにボタンタグを追加し、onClick属性に先程の関数(onClickDetailA)を定義
    
    Page1DetailA.jsxを編集
    コンポーネント内でuseHistoryフックを使用し、historyインスタンスを生成
    その後、関数(onClickBack)を定義し、historyインスタンスのgoBackメソッドを実行するよう記述
    返り値のjsxにボタンタグを追加し、onClick属性に先程の関数(onClickBack)を定義
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823434
    https://v5.reactrouter.com/web/api/Hooks/usehistory
    https://v5.reactrouter.com/web/api/history

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push('/page1/detailA', arr);
 
   return (
     <StyledPage1>
@@ -15,6 +19,7 @@ export const Page1 = () => {
         DetailA
       </Link>
       <Link to="/page1/detailB">DetailB</Link>
+      <button onClick={onClickDetailA}>DetailA</button>
     </StyledPage1>
   );
 }
@@ -22,4 +27,6 @@ export const Page1 = () => {
 const StyledPage1 = styled.div`
   display: flex;
   flex-direction: column;
+  margin: 0 auto;
+  width: 40vw
 `;
