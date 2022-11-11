c6e49d2692d70b235729f969bb499eb391d79a3c
the-bears-field <thebearsfield+github@gmail.com>
2022-11-11 09:51:51 +0900

    Atomic Design学習に向けて初期化

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import { BrowserRouter, Link } from 'react-router-dom';
-import React from 'react';
 import './App.css';
-import styled from 'styled-components';
-import { Router } from './router/Router';
-
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <StyledApp>
-        <Link to="/">Home</Link>
-        <Link to="/page1">Page1</Link>
-        <Link to="/page2">Page2</Link>
-        <Router />
-      </StyledApp>
-    </BrowserRouter>
+    <>
+    </>
   );
 }
-
-const StyledApp = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-`;
