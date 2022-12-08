ddc99b3aa49abc995e1d78186890f9722e431612
the-bears-field <thebearsfield+github@gmail.com>
2022-12-08 15:01:34 +0900

    Reactでの非同期処理について学習
    Top.jsxにaxiosをインポート
    非同期処理のための関数を追加し、Axiosを用いた処理を記述
    jsxに非同期処理用のボタンを設置し、onClick属性の値に非同期処理のための関数を設定
    
    参考URL
    https://axios-http.com/docs/example
    https://jsonplaceholder.typicode.com/
    https://jsprimer.net/basic/async/#promise

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { SecondaryButton } from '../atoms/button/SecondaryButton';
 import { useSetRecoilState } from "recoil";
 import { userState } from '../../store/userState';
+import axios from "axios";
 
 
 export const Top = () => {
@@ -19,12 +20,36 @@ export const Top = () => {
     history.push('/users');
   };
 
+  const onClickUsers = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const onClickUser1 = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1')
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <StyledContainer>
       <h2>TOPページです</h2>
       <ButtonWrapper>
         <SecondaryButton className="button--admin" onClick={onClickAdmin}>管理者</SecondaryButton>
         <SecondaryButton className="button--general" onClick={onClickGeneral}>一般ユーザ</SecondaryButton>
+      </ButtonWrapper>
+      <ButtonWrapper>
+        <SecondaryButton className="button--users" onClick={onClickUsers}>Users</SecondaryButton>
+        <SecondaryButton className="button--user1" onClick={onClickUser1}>User1</SecondaryButton>
       </ButtonWrapper>
     </StyledContainer>
   );
@@ -38,6 +63,6 @@ const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-  margin: 0 auto;
+  margin: 48px auto 0;
   width: 128px;
 `;
