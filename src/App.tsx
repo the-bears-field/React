09a1a9244b6c4aa803585e1d98ca85a3b61931a5
the-bears-field <thebearsfield+github@gmail.com>
2023-01-05 14:56:10 +0900

    オプショナルチェーンを用いてnull安全なコーディングを学習
    typesディレクトリ下にUser.tsを作成し、User型を定義
    hobbiesプロパティはオプショナルとした。
    
    srcディレクトリ配下にUserProfile.tsxを作成
    ファイル中でProps型を定義あい、UserProfileコンポーネントを作成
    コンポーネント自体はVFC型、引数(props)はProps型を使用。
    propsの内容を変数(user)に格納した上で、JSX中で内容を出力
    オプショナルプロパティである配列の出力処理にオプショナルチェーン(?.演算子)を使用し、プロパティ参照エラーを回避するように対応
    
    App.tsx編集
    UserProfileコンポーネント、User型をインポート
    ユーザ情報を変数に格納
    Appコンポーネント中で、UserProfileを出力
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823596
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Optional_chaining
    https://typescriptbook.jp/reference/values-types-variables/object/optional-chaining
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/join

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { useState } from 'react';
 import { Todo } from './Todo'
 import { TodoType } from './types/Todo'
 import { Text } from './Text'
+import { UserProfile } from './UserProfile';
+import { User } from './types/User';
+
+const user: User = {
+  name: 'John Smith',
+  hobbies: ['ネットサーフィン', 'クラッキング']
+}
 
 export const App = () => {
   const [ todos, setTodos ] = useState<TodoType[]>([])
@@ -19,6 +26,7 @@ export const App = () => {
 
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" >テキストです</Text>
       <button onClick={onClickFetchData}>データ取得</button>
       { todos.map((todo) => (
