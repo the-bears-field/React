a4a934dc113329babbeba240834efeb26e316b40
the-bears-field <thebearsfield+github@gmail.com>
2023-01-06 14:39:09 +0900

    カスタムフック学習に向けて初期化

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,23 @@
+import logo from './logo.svg';
 import './styles.css';
-import axios from 'axios';
-import { useState } from 'react';
-import { Todo } from './Todo'
-import { TodoType } from './types/Todo'
-import { Text } from './Text'
-import { UserProfile } from './UserProfile';
-import { User } from './types/User';
-
-const user: User = {
-  name: 'John Smith',
-  hobbies: ['ネットサーフィン', 'クラッキング']
-}
 
 export const App = () => {
-  const [ todos, setTodos ] = useState<TodoType[]>([])
-  const onClickFetchData = () => {
-    axios
-      .get<TodoType[]>('https://jsonplaceholder.typicode.com/todos')
-      .then((response) => {
-        setTodos(response.data)
-      }).catch((error) => {
-        console.log(error)
-      })
-  }
-
   return (
     <div className="App">
-      <UserProfile user={user} />
-      <Text color="red" fontSize="18px" >テキストです</Text>
-      <button onClick={onClickFetchData}>データ取得</button>
-      { todos.map((todo) => (
-        <Todo
-          key={todo.id}
-          title={todo.title}
-          userId={todo.userId}
-          completed={todo.completed}
-        />
-      )) }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
