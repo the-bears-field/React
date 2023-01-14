6e14a153f1cb6b1904f5d31e2405dc9640516964
the-bears-field <thebearsfield+github@gmail.com>
2023-01-14 17:26:49 +0900

    カスタムフック未使用でのデータ取得、出力する処理を構築
    styles.css編集
    AppクラスのCSSを削除
    
    tsconfig.json編集
    promise.finallyを使用するため、compilerOptions>libに記述してあるバージョンをes2018に変更
    
    型定義のファイル(userProfile.ts, user.ts)を新規作成
    srcディレクトリ下にtypesディレクトリを新規作成
    typesディレクトリ下にuserProfile.tsを作成し、必要な要素の型を定義
    typesディレクトリ下にapiディレクトリ新規作成
    apiディレクトリ下にuser.tsを作成し、APIデータを元に型定義
    
    srcディレクトリ下にcomponentsディレクトリを作成し、その配下にUserCard.tsxを新規作成
    UserCardコンポーネントはVFC型、引数はProps型として定義
    
    App.tsx編集
    Appコンポーネントに必要な要素をインポート
    Appコンポーネントの型をVFC型として定義
    ボタンを押したときの処理(onClickFetchUser)を定義
    useStateフック、ジェネリクスを使い変数(userProfiles, isLoading, isError)を定義
    userProfilesは配列型で内容はUserProfile型、
    isLoadingとisErrorはboolean型で定義
    axiosのgetメソッドでユーザ情報を取得し、取得成功時、取得失敗時、プロミス決定後の処理を記述
    AppコンポーネントのJSX内で、変数(userProfiles, isLoading, isError)の内容により出力内容を変更
    isLoadingとisErrorが共にfalseであった場合、mapメソッドを使いuserProfilesの内容を展開し出力
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823648
    https://www.typescriptlang.org/ja/tsconfig#lib
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise/finally

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './styles.css';
+import { UserCard } from './components/UserCard';
+import { useState, VFC } from 'react';
+import axios from 'axios';
+import { UserProfile } from './types/userProfile';
+import { User } from './types/api/user';
+import styled from 'styled-components';
 
-export const App = () => {
+export const App: VFC = () => {
+  const [ userProfiles, setUserProfiles ] = useState<Array<UserProfile>>([])
+  const [ isLoading, setIsLoading ] = useState<boolean>(false)
+  const [ isError, setIsError ] = useState<boolean>(false)
+
+  const onClickFetchUser = () => {
+    setIsLoading(true)
+    setIsError(false)
+
+    axios
+      .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        const data = res.data.map<UserProfile>((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.street} ${user.address.suite} ${user.address.city}`
+        }))
+        setUserProfiles(data)
+      }).catch(() => {
+        setIsError(true)
+      }).finally(() => {
+        setIsLoading(false)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      { isError && <StyledErrorMessage>データ取得に失敗しました</StyledErrorMessage> }
+      { isLoading && <StyledParagraph>Loading...</StyledParagraph> }
+      { (!isError && !isLoading) &&
+        userProfiles.map((user) => <UserCard key={user.id} user={user} />)
+      }
+    </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  width:100vw;
+  margin: 0 auto;
+`
+
+const StyledParagraph = styled.p`
+  color: black;
+  font-size: 16px;
+`
+
+const StyledErrorMessage = styled(StyledParagraph)`
+  color: red;
+`
