5d605ccd640e7d1cf76e45cd93b3cb4d4b630896
the-bears-field <thebearsfield+github@gmail.com>
2023-01-20 14:16:12 +0900

    グローバルなスタイル定義の方法を学習
    srcディレクトリ下にthemeディレクトリを新規作成
    themeディレクトリ下にtheme.tsを作成
    extendTheme関数を@chakra-ui/reactからインポート
    extendTheme関数の返り値を変数(theme)に格納しエクスポート
    
    App.tsx編集
    変数(theme)をインポート
    不要なインポート処理を削除
    ChakraProviderにtheme属性を付与し、値にインポートした変数(theme)を指定
    
    参考URL
    https://www.udemy.com/course/react_stepup/learn/lecture/24823696
    https://v1.chakra-ui.com/docs/styled-system/theming/customize-theme
    https://v1.chakra-ui.com/docs/styled-system/features/global-styles
    https://v1.chakra-ui.com/docs/styled-system/theming/theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './styles.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme/theme';
 
 export const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">button</Button>
     </ChakraProvider>
   );
