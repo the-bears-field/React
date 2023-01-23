70bdcb323b57bc59adbdfb046371b5b6b16280e5
the-bears-field <thebearsfield+github@gmail.com>
2023-01-23 14:21:08 +0900

    厳格な型チェックを有効化 一部型定義を修正

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-const rootElement: HTMLElement = document.getElementById("root");
+const rootElement: HTMLElement | null = document.getElementById("root");
 
 ReactDOM.render(
   <StrictMode>
